601701a1f48442926809d533140b0b44c13c4213
Michel <Michel@Michel-PC>
2017-05-19 16:51:54 -0300

    manipulando datas

--- src/manipulandodatas/metodos/MetodosCalendar.java
+++ src/manipulandodatas/metodos/MetodosCalendar.java
@@ -8,6 +8,11 @@ package manipulandodatas.metodos;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
 
 /**
  *
@@ -179,7 +184,197 @@ public class MetodosCalendar extends GregorianCalendar {
         System.out.println("pegar HOUR>>> " + hoje.get(HOUR));
         System.out.println("pegar HOUR_OF_DAY>>> " + hoje.get(HOUR_OF_DAY));
     }
-    
-    
+
+    public void pegarMaximo() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("pegar>>> " + hoje.getTime());
+        System.out.println("pegar YEAR>>> " + hoje.getMaximum(YEAR));
+        System.out.println("pegar DATE>>> " + hoje.getMaximum(DATE));
+        System.out.println("pegar MONTH>>> " + hoje.getMaximum(MONTH));
+        System.out.println("pegar DAY_OF_MONTH>>> " + hoje.getMaximum(DAY_OF_MONTH));
+        System.out.println("pegar HOUR>>> " + hoje.getMaximum(HOUR));
+        System.out.println("pegar HOUR_OF_DAY>>> " + hoje.getMaximum(HOUR_OF_DAY));
+    }
+
+    public void pegarMinimo() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("pegar>>> " + hoje.getTime());
+        System.out.println("pegar YEAR>>> " + hoje.getMinimum(YEAR));
+        System.out.println("pegar DATE>>> " + hoje.getMinimum(DATE));
+        System.out.println("pegar MONTH>>> " + hoje.getMinimum(MONTH));
+        System.out.println("pegar DAY_OF_MONTH>>> " + hoje.getMinimum(DAY_OF_MONTH));
+        System.out.println("pegar HOUR>>> " + hoje.getMinimum(HOUR));
+        System.out.println("pegar HOUR_OF_DAY>>> " + hoje.getMinimum(HOUR_OF_DAY));
+    }
+
+    public void tiposDeCalendariosAvaliados() {
+        Set set = Calendar.getAvailableCalendarTypes();
+        set.stream().forEach((s) -> {
+            System.out.println(s);
+        });
+        // import java.time.chrono;
+        //Class ThaiBuddhistChronology;
+        //Class JapaneseChronology
+        //Class JapaneseDate
+    }
+
+    public void locaisAvaliados() {
+        Locale[] l = Calendar.getAvailableLocales();
+        for (Locale lo : l) {
+            System.out.println(lo);
+        }
+    }
+
+    public void tipoCalendario() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("tipo calendário>>> " + hoje.getCalendarType());
+    }
+
+    public void mostrarNome() {
+        //https://www.tutorialspoint.com/java/util/calendar_getdisplayname.htm
+        Calendar hoje = dataDeHoje();
+        System.out.println("hoje " + hoje.getTime());
+        System.out.println("mostrarNome>>> " + hoje.getDisplayName(DAY_OF_WEEK, 2, Locale.US));
+        /*
+        field -- the calendar field.
+        style -- the style that will be applied to the string representations
+        locale -- the string representation locale
+         */
+    }
+
+    public void mostrarNomes() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("hoje " + hoje.getTime());
+        Map<String, Integer> map = hoje.getDisplayNames(DAY_OF_WEEK, 2, Locale.US);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+        }
+    }
+
+    public void primeiroDiaDaSemana() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("primeiroDiaDaSemana>>> " + hoje.getFirstDayOfWeek());
+    }
+
+    public void oMaiorNumeroMinimo() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("oMaiorNumeroMinimo>>> " + hoje.getGreatestMinimum(DAY_OF_WEEK));
+    }
+
+    public void outraInstaciaDeLocal() {
+        Calendar hoje = Calendar.getInstance(Locale.CANADA);
+        System.out.println("outraInstaciaDeLocal>> " + hoje.getTime());
+    }
+
+    public void instanciaDeTempo() {
+        TimeZone timeZone = TimeZone.getTimeZone("UTC");
+        Calendar calendar = Calendar.getInstance(timeZone);
+        SimpleDateFormat simpleDateFormat
+                = new SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy", Locale.US);
+        simpleDateFormat.setTimeZone(timeZone);
+        System.out.println("instanciaDeTempo>> " + calendar.getTime());
+        System.out.println("UTC:     " + simpleDateFormat.format(calendar.getTime()));
+    }
+
+    public void instanciaDeTempoLocal() {
+        TimeZone timeZone = TimeZone.getTimeZone("UTC");
+        Calendar calendar = Calendar.getInstance(timeZone, Locale.US);
+        SimpleDateFormat simpleDateFormat
+                = new SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy");
+        simpleDateFormat.setTimeZone(timeZone);
+        System.out.println("instanciaDeTempo>> " + calendar.getTime());
+        System.out.println("UTC:     " + simpleDateFormat.format(calendar.getTime()));
+    }
+
+    public void peloMenosOMaximo() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("peloMenosOMaximo>>> " + hoje.getLeastMaximum(DAY_OF_WEEK));
+    }
+
+    public void oMaximo() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("oMaximo>>> " + hoje.getMaximum(DAY_OF_WEEK));
+    }
+
+    public void oMinimo() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("oMinimo>>> " + hoje.getMinimum(DAY_OF_WEEK));
+    }
+
+    public void oPrimeiroDiaDaPrimeiraSemana() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("oPrimeiroDiaDaPrimeiraSemana>>> " + hoje.getMinimalDaysInFirstWeek());
+    }
+
+    public void capturaAData() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("capturaAData>>> " + hoje.getTime());
+    }
+
+    public void retornaOsMilissegundos() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("retornaOsMilissegundos>>> " + hoje.getTimeInMillis());
+    }
+
+    public void retornaTimeZone() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("retornaTimeZone>>> " + hoje.getTimeZone());
+        System.out.println("retornaTimeZone>>> " + hoje.getTimeZone().getDisplayName());
+    }
+
+    public void numerosDeSemanasNoAno() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("numerosDeSemanasNoAno>>> " + hoje.getWeeksInWeekYear());
+    }
+
+    public void retornaOAno() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("retornaOAno>>> " + hoje.getWeekYear());
+    }
+
+    public void codigoHash() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("codigoHash>>> " + hoje.hashCode());
+    }
+
+    public void capturaInterna() {
+        MetodosCalendar mc = new MetodosCalendar();
+        System.out.println("capturaInterna>>> " + mc.internalGet(DAY_OF_WEEK));
+    }
+
+    public void leniente() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("leniente>>> " + hoje.isLenient());
+    }
+
+    public void ehSet() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("ehSet>>> " + hoje.isSet(DAY_OF_WEEK));
+        hoje.clear();
+        System.out.println("ehSet>>> " + hoje.isSet(DAY_OF_WEEK));
+    }
+
+    public void estaSemanaESuportada() {
+        Calendar hoje = dataDeHoje();
+        System.out.println("estaSemanaESuportada>>> " + hoje.isWeekDateSupported());
+    }
+
+    public void rolar() {
+        Calendar hoje = dataDeHoje();
+        hoje.roll(Calendar.MONTH, true);
+        System.out.println("Para frente>>> " + hoje.getTime());
+        Calendar hoje1 = dataDeHoje();
+        hoje1.roll(Calendar.MONTH, false);
+        System.out.println("Para trás>>> " + hoje1.getTime());
+    }
+
+    public void rolar1() {
+        Calendar hoje = dataDeHoje();
+        hoje.roll(Calendar.MONTH, 1);
+        System.out.println("Para frente>>> " + hoje.getTime());
+        Calendar hoje1 = dataDeHoje();
+        hoje1.roll(Calendar.MONTH, -1);
+        System.out.println("Para trás>>> " + hoje1.getTime());
+    }
 
 }
